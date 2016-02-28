6ed2193cb05b495490c00d0a689fdcf936c7cc2b
Shikib Mehri <mehrishikib@gmail.com>
2016-02-28 02:23:38 -0800

    added 3 col functionality for newly retrieved posts

--- js/index.js
+++ js/index.js
@@ -20,8 +20,34 @@ function retrieve_posts() {
     posts = posts.concat(new_posts); 
     post_count = posts.length;
     for(var i = 0; i < new_posts.length; i++) {
-      $("#card-view").append("<div class='item'>" + new_posts[i].text + "</div>");
+      $("#card-view").append("<div class='item'>" + 
+                               "<div class='flex-container'>" +
+                                  "<div class='small-item left'>Left</div>" +
+                                  "<div class='large-item middle'>" + new_posts[i].text + "</div>" +
+                                  "<div class='small-item right'>Right</div>" +
+                                "</div>" +
+                              "</div>");
     }
+
+    set_click_events();
+  });
+}
+
+function set_click_events() {
+  $(".left").click(function(){
+    $(this).css("flex-grow",2);
+    $(this).parent().children(".middle").css("flex-grow",1);
+    $(this).parent().children(".right").css("flex-grow",1);
+  });
+  $(".middle").click(function(){
+    $(this).css("flex-grow",2);
+    $(this).parent().children(".left").css("flex-grow",1);
+    $(this).parent().children(".right").css("flex-grow",1);
+  });
+  $(".right").click(function(){
+    $(this).css("flex-grow",2);
+    $(this).parent().children(".left").css("flex-grow",1);
+    $(this).parent().children(".middle").css("flex-grow",1);
   });
 }
 
